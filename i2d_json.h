1a8931bec3df419799d7fc2c05720b85bef8a925
trickyloki3 <tricky.loki3@gmail.com>
2018-02-12 02:02:33 -0500

    * Implemented i2d_keywords.

--- i2d_json.h
+++ i2d_json.h
@@ -2,12 +2,22 @@
 #define i2d_json_h
 
 #include "i2d_util.h"
+#include "i2d_rbt.h"
+#include "jansson.h"
+
+struct i2d_keywords {
+    i2d_str * list;
+    size_t size;
+    i2d_rbt * index;
+};
+
+typedef struct i2d_keywords i2d_keywords;
+
+int i2d_keywords_init(i2d_keywords **, json_t *);
+void i2d_keywords_deit(i2d_keywords **);
 
 struct i2d_json {
-    struct {
-        i2d_str * list;
-        size_t size;
-    } keywords;
+    i2d_keywords * keywords;
 };
 
 typedef struct i2d_json i2d_json;
