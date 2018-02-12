762539b0f80263f4afbdd4b648dfd6af5304f9b8
trickyloki3 <tricky.loki3@gmail.com>
2018-02-11 22:25:37 -0500

    * Test lexer on item db.

--- i2d.c
+++ i2d.c
@@ -6,10 +6,31 @@
 
 int main(int argc, char * argv[]) {
     int status = I2D_OK;
+    i2d_option * option = NULL;
+    i2d_item_db * item_db = NULL;
+    i2d_lexer * lexer = NULL;
+    i2d_item * item = NULL;
 
-#if i2d_debug
-    status = i2d_lexer_test();
-#endif
+    if(i2d_option_init(&option, argc, argv)) {
+        status = i2d_panic("failed to create option object");
+    } else {
+        if(i2d_item_db_init(&item_db, option->item_db_path)) {
+            status = i2d_panic("failed to create item database object");
+        } else {
+            if(i2d_lexer_init(&lexer)) {
+                status = i2d_panic("failed to create lexer object");
+            } else {
+                item = item_db->list->next;
+                while(item != item_db->list) {
+                    i2d_lexer_tokenize(lexer, item->script);
+                    item = item->next;
+                }
+                i2d_lexer_deit(&lexer);
+            }
+            i2d_item_db_deit(&item_db);
+        }
+        i2d_option_deit(&option);
+    }
 
     return status;
 }
