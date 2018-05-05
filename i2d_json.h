bcaaeae93e1528bff9b8b8e1d7fb5a2d32db4903
trickyloki3 <tricky.loki3@gmail.com>
2018-05-05 19:44:06 -0400

    * Revise i2d_json.c and i2d_json.h.

--- i2d_json.h
+++ i2d_json.h
@@ -2,53 +2,8 @@
 #define i2d_json_h
 
 #include "i2d_util.h"
-#include "i2d_rbt.h"
 #include "jansson.h"
 
-typedef int (*i2d_object_create) (void **, const char *, json_t *, i2d_rbt *, void *);
-typedef void (*i2d_object_delete) (void **);
-
-struct i2d_object {
-    i2d_rbt * map;
-    void ** list;
-    size_t size;
-    i2d_object_create create;
-    i2d_object_delete delete;
-};
-
-typedef struct i2d_object i2d_object;
-
-int i2d_object_init(i2d_object **, json_t *, const char *, i2d_object_create, i2d_object_delete, i2d_rbt_cmp, void *);
-void i2d_object_deit(i2d_object **);
-int i2d_object_map(i2d_object *, void *, void **);
-
-struct i2d_json {
-    json_t * object;
-    json_t * blocks;
-};
-
-typedef struct i2d_json i2d_json;
-
-int i2d_json_init(i2d_json **, i2d_str *);
-void i2d_json_deit(i2d_json **);
-
-int i2d_json_object_list(json_t *, void ***, size_t *);
-int i2d_json_array_list(json_t *, void ***, size_t *);
-int i2d_json_get_object(json_t *, const char *, json_t **);
-int i2d_json_get_str(json_t *, const char *, i2d_str_const *);
-int i2d_json_get_int(json_t *, const char *, json_int_t *);
-
-struct i2d_str_list {
-    i2d_str ** list;
-    size_t size;
-};
-
-typedef struct i2d_str_list i2d_str_list;
-
-int i2d_str_list_init(i2d_str_list **, const char *, json_t *);
-void i2d_str_list_deit(i2d_str_list **);
-
-#if i2d_debug
-int i2d_json_test();
-#endif
+int i2d_json_create(json_t *, i2d_string *);
+void i2d_json_destroy(json_t *);
 #endif
