39c6deb9b2d29d9f2038aad223944fd18cb07f74
trickyloki3 <tricky.loki3@gmail.com>
2018-08-26 19:55:40 -0400

    * Added itemheal statement support.

--- i2d_script.c
+++ i2d_script.c
@@ -2509,6 +2509,7 @@ int i2d_script_statement(i2d_script * script, i2d_block * block, i2d_rbt * varia
         case I2D_AUTOBONUS3:
         case I2D_HEAL:
         case I2D_PERCENTHEAL:
+        case I2D_ITEMHEAL:
             status = i2d_script_statement_generic(script, block);
             break;
         default:
