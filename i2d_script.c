ed60b858792ede9d06a79e7e81a8d5431e5a6aa9
trickyloki3 <tricky.loki3@gmail.com>
2018-08-26 22:10:28 -0400

    * Added unitskilluseid statement support.

--- i2d_script.c
+++ i2d_script.c
@@ -2517,6 +2517,7 @@ int i2d_script_statement(i2d_script * script, i2d_block * block, i2d_rbt * varia
         case I2D_ITEMHEAL:
         case I2D_SKILL:
         case I2D_ITEMSKILL:
+        case I2D_UNITSKILLUSEID:
             status = i2d_script_statement_generic(script, block);
             break;
         default:
