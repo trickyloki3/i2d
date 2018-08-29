bc2d2af3a216a47b736d0dbe82486185a93ba78d
trickyloki3 <tricky.loki3@gmail.com>
2018-08-28 23:13:48 -0400

    * Added homevolution statement support.

--- i2d_script.c
+++ i2d_script.c
@@ -2554,6 +2554,7 @@ int i2d_script_statement(i2d_script * script, i2d_block * block, i2d_rbt * varia
         case I2D_BUYINGSTORE:
         case I2D_SEARCHSTORES:
         case I2D_ANNOUNCE:
+        case I2D_HOMEVOLUTION:
             status = i2d_script_statement_generic(script, block);
             break;
         /* statement without description */
