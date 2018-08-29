95b2ed693b23edc06e810602701f8370b430db6f
trickyloki3 <tricky.loki3@gmail.com>
2018-08-29 01:01:38 -0400

    * Added mercenary_heal statement support.

--- i2d_script.c
+++ i2d_script.c
@@ -2560,6 +2560,7 @@ int i2d_script_statement(i2d_script * script, i2d_block * block, i2d_rbt * varia
         case I2D_BPET:
         case I2D_GETGROUPITEM:
         case I2D_MERCENARY_CREATE:
+        case I2D_MERCENARY_HEAL:
             status = i2d_script_statement_generic(script, block);
             break;
         /* statement without description */
