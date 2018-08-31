47a981ff2955aa63d670ec6c79c9080888fc7d1e
trickyloki3 <tricky.loki3@gmail.com>
2018-08-31 03:59:36 -0400

    * Added transform statement support.

--- i2d_script.c
+++ i2d_script.c
@@ -2590,6 +2590,7 @@ int i2d_script_statement(i2d_script * script, i2d_block * block, i2d_rbt * varia
         case I2D_PETSKILLATTACK2:
         case I2D_PETSKILLATTACK:
         case I2D_GETRANDGROUPITEM:
+        case I2D_TRANSFORM:
             status = i2d_script_statement_generic(script, block);
             break;
         /* statement without description */
