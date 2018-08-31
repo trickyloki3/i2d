66d15c84b52fa13ce48d0e262ace5e594cc38685
trickyloki3 <tricky.loki3@gmail.com>
2018-08-31 02:08:48 -0400

    * Added petskillattack2 statement support.

--- i2d_script.c
+++ i2d_script.c
@@ -2587,6 +2587,7 @@ int i2d_script_statement(i2d_script * script, i2d_block * block, i2d_rbt * varia
         case I2D_COOKING:
         case I2D_PETLOOT:
         case I2D_PETSKILLSUPPORT:
+        case I2D_PETSKILLATTACK2:
             status = i2d_script_statement_generic(script, block);
             break;
         /* statement without description */
