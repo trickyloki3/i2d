b865dcbffbd8a5eea328acd11d695c4a93bccc18
trickyloki3 <tricky.loki3@gmail.com>
2018-08-30 23:04:38 -0400

    * Added petloot statement support.

--- i2d_script.c
+++ i2d_script.c
@@ -2585,6 +2585,7 @@ int i2d_script_statement(i2d_script * script, i2d_block * block, i2d_rbt * varia
         case I2D_PET:
         case I2D_PRODUCE:
         case I2D_COOKING:
+        case I2D_PETLOOT:
             status = i2d_script_statement_generic(script, block);
             break;
         /* statement without description */
