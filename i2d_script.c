9154a28024dcf66000982a44ea66813d12795e28
trickyloki3 <tricky.loki3@gmail.com>
2018-09-02 06:08:38 -0400

    * Added setmadogear statement support.

--- i2d_script.c
+++ i2d_script.c
@@ -2664,6 +2664,7 @@ int i2d_script_statement(i2d_script * script, i2d_block * block, i2d_rbt * varia
         case I2D_GETRANDGROUPITEM:
         case I2D_TRANSFORM:
         case I2D_SETFALCON:
+        case I2D_SETMADOGEAR:
             status = i2d_script_statement_generic(script, block);
             break;
         /* statement without description */
