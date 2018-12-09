ae6f62e592027765a70d2865180068506268d724
trickyloki3 <tricky.loki3@gmail.com>
2018-12-09 03:03:45 -0500

    * Ignore printing output on debug build.

--- i2d.c
+++ i2d.c
@@ -63,6 +63,7 @@ static int i2d_item_compile_script(i2d_script * script, i2d_item * item) {
             if(i2d_script_compile(script, &item->onunequip_script, &onunequip_script, NULL)) {
                 status = i2d_panic("failed to translate onunequip script for item %ld", item->id);
             } else {
+#ifndef _DEBUG
                 fprintf(stdout, "%ld - %s\n", item->id, item->name.string);
                 if(normal_script.length) 
                     fprintf(stdout, "[script]\n%s", normal_script.string);
@@ -73,6 +74,7 @@ static int i2d_item_compile_script(i2d_script * script, i2d_item * item) {
                 if(i2d_item_compile_combo_script(script, item))
                     status = i2d_panic("failed to translate item combo script(s) for item %ld", item->id);
                 fprintf(stdout, "~\n");
+#endif
                 i2d_string_destroy(&onunequip_script);
             }
             i2d_string_destroy(&onequip_script);
